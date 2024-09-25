1adb6d33cc262d5fe30315f1b96bdad027594afa
patrick.deschambault <patrick.deschambault@haply.co>
2024-09-25 09:02:34 -0400

    Print values and enable pwm to a dimmable one

--- raspberry-pi/examples/adc.rs
+++ raspberry-pi/examples/adc.rs
@@ -1,35 +1,51 @@
-use std::{process::exit, sync::{Arc, Mutex}, thread, time::Duration};
+use std::{f64::consts::LOG10_2, process::exit, sync::{Arc, Mutex}, thread, time::Duration};
 
 use linux_embedded_hal::I2cdev;
 use raspberry_pi::adc7830::{values, I2C_DEV_PATH};
 use rppal::pwm::Polarity;
 use rppal::pwm::{Channel, Pwm};
 
+const STEPS: f64 = 255.0;
+
 fn main() {
     
     let bus = Arc::new(Mutex::new(I2cdev::new(I2C_DEV_PATH).unwrap()));
-    //let pwm = Pwm::with_period(Channel::Pwm0, Duration::from_millis(100), Duration::from_millis(1), Polarity::Normal, true).unwrap();
-    //let pwm = Arc::new(pwm);
-    
-    let bus_clone = Arc::clone(&bus);
+    let pwm = Pwm::with_period(Channel::Pwm0, Duration::from_millis(1), Duration::from_millis(1), Polarity::Normal, true).unwrap();
+    let pwm = Arc::new(pwm);
+
 
     ctrlc::set_handler({
-        //let pwm = pwm.clone();
+        let pwm = pwm.clone();
         move || {
         println!("Exiting gracefully...");
-        //pwm.disable().unwrap();
+        pwm.disable().unwrap();
         exit(0);
     }}).expect("Error setting Ctrl-C handler");
 
     thread::spawn(move || {
-        for value in values(bus_clone, 0) {
-            //pwm.set_duty_cycle(value as f64).unwrap();
+
+        loop {
+            let value = values(bus.clone(), 0);
+
+            if let Some(v) = value {
+                let fade_factor = fade_factor();
+                let duty = (2f64.powf(v as f64 / fade_factor) - 1.0) / STEPS;
+                pwm.set_duty_cycle(duty).unwrap();
+                println!("{:?}, {:?}", v, duty);
+            }
+        
             thread::sleep(Duration::from_millis(50));
+
         }
+        
     });
 
     // Infinite loop to keep the program running
     loop {
         thread::sleep(Duration::from_secs(1));
     }
+}
+
+fn fade_factor() -> f64 {
+    (STEPS * LOG10_2) / f64::log10(STEPS)
 }
