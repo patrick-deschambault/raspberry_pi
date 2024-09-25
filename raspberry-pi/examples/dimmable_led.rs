a78b15e33e937d409020e72d643d96bbc7a79f84
patrick.deschambault <patrick.deschambault@haply.co>
2024-09-25 09:05:18 -0400

    cargo fmt

--- raspberry-pi/examples/dimmable_led.rs
+++ raspberry-pi/examples/dimmable_led.rs
@@ -1,4 +1,10 @@
-use std::{f64::consts::LOG10_2, process::exit, sync::{Arc, Mutex}, thread, time::Duration};
+use std::{
+    f64::consts::LOG10_2,
+    process::exit,
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
 
 use linux_embedded_hal::I2cdev;
 use raspberry_pi::ads7830::{values, I2C_DEV_PATH};
@@ -8,36 +14,38 @@ use rppal::pwm::{Channel, Pwm};
 const STEPS: f64 = 255.0;
 
 fn main() {
-    
     let bus = Arc::new(Mutex::new(I2cdev::new(I2C_DEV_PATH).unwrap()));
-    let pwm = Pwm::with_period(Channel::Pwm0, Duration::from_millis(1), Duration::from_millis(1), Polarity::Normal, true).unwrap();
+    let pwm = Pwm::with_period(
+        Channel::Pwm0,
+        Duration::from_millis(1),
+        Duration::from_millis(1),
+        Polarity::Normal,
+        true,
+    )
+    .unwrap();
     let pwm = Arc::new(pwm);
-
 
     ctrlc::set_handler({
         let pwm = pwm.clone();
         move || {
-        println!("Exiting gracefully...");
-        pwm.disable().unwrap();
-        exit(0);
-    }}).expect("Error setting Ctrl-C handler");
-
-    thread::spawn(move || {
-
-        loop {
-            let value = values(bus.clone(), 0);
-
-            if let Some(v) = value {
-                let fade_factor = fade_factor();
-                let duty = (2f64.powf(v as f64 / fade_factor) - 1.0) / STEPS;
-                pwm.set_duty_cycle(duty).unwrap();
-                println!("{:?}, {:?}", v, duty);
-            }
-        
-            thread::sleep(Duration::from_millis(50));
-
+            println!("Exiting gracefully...");
+            pwm.disable().unwrap();
+            exit(0);
         }
-        
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    thread::spawn(move || loop {
+        let value = values(bus.clone(), 0);
+
+        if let Some(v) = value {
+            let fade_factor = fade_factor();
+            let duty = (2f64.powf(v as f64 / fade_factor) - 1.0) / STEPS;
+            pwm.set_duty_cycle(duty).unwrap();
+            println!("{:?}, {:?}", v, duty);
+        }
+
+        thread::sleep(Duration::from_millis(50));
     });
 
     // Infinite loop to keep the program running
--- raspberry-pi/src/ads7830.rs
+++ raspberry-pi/src/ads7830.rs
@@ -1,10 +1,9 @@
-use linux_embedded_hal::I2cdev;
 use embedded_hal::i2c::I2c;
+use linux_embedded_hal::I2cdev;
 use std::sync::{Arc, Mutex};
 
 pub const I2C_DEV_PATH: &str = "/dev/i2c-1"; // Use correct I2C bus here
 pub const ADS7830_ADDR: u16 = 0x4b;
-
 
 fn ads7830_commands() -> [u8; 8] {
     [0x84, 0xc4, 0x94, 0xd4, 0xa4, 0xe4, 0xb4, 0xf4]
@@ -19,16 +18,13 @@ fn read_ads7830(bus: &mut I2cdev, input: usize) -> Result<u8, std::io::Error> {
     bus.write_read(ADS7830_ADDR, &write, &mut buffer).unwrap();
 
     Ok(buffer[0])
-    
 }
 
-pub fn values(bus: Arc<Mutex<I2cdev>>, input: usize) -> Option<u8> {    
-        let mut bus = bus.lock().unwrap();
+pub fn values(bus: Arc<Mutex<I2cdev>>, input: usize) -> Option<u8> {
+    let mut bus = bus.lock().unwrap();
 
-        match read_ads7830(&mut bus, input) {
-            Ok(value) => Some(value),
-            Err(_) => None,
-        }   
+    match read_ads7830(&mut bus, input) {
+        Ok(value) => Some(value),
+        Err(_) => None,
+    }
 }
-
-
